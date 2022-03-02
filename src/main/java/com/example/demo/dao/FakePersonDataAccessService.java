7e0358a29e827ef1c2b0a802a541f478bff5a25d
dhwo0787 <dhwo0787@naver.com>
2022-03-03 01:21:51 +0900

    head to the first

---
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -0,0 +1,24 @@
+package com.example.demo.dao;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import com.example.demo.model.Person;
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao {
+	
+	private static List<Person> DB = new ArrayList<>();
+
+	@Override
+	public int insertPerson(UUID id, Person person) {
+		DB.add(new Person(id,person.getName()));
+		return 1;
+	}
+	
+
+}
